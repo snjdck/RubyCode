e03d9db2abf5a32235196ba4c7f3e19f17d95c81
Alex Shao <shao_kai@qq.com>
2017-01-10 00:21:38 +0800

    update

--- gems/reactor/lib/reactor/socket_handler.rb
+++ gems/reactor/lib/reactor/socket_handler.rb
@@ -1,12 +1,8 @@
-module SocketHandler
-	def send_packet packet
-		send_raw packet.to_s
-	end
-
+module SocketHandlerBase
 	def send_raw str
 		return if closed? || str.empty?
 		@buffer_send ||= ''
-		unless @buffer_send.bytesize > 0
+		if @buffer_send.empty?
 			$selector.add_send self
 		end
 		@buffer_send << str
@@ -24,12 +20,7 @@ module SocketHandler
 
 	def on_recv
 		(@buffer_recv ||= '') << recv(4096)
-		while packet = Packet.parse(@buffer_recv)
-			case packet.type
-			when Packet::TYPE_DEFAULT then on_packet  packet
-			when Packet::TYPE_CONTROL then on_control packet
-			end
-		end
+		on_parse
 	end
 
 	def close
@@ -43,8 +34,25 @@ module SocketHandler
 		$stdout.puts obj
 	end
 
-	def on_packet (packet); end
-	def on_control(packet); end
+	def on_parse; end
 	def on_connect; end
 	def on_disconnect; end
+end
+
+module SocketHandler include SocketHandlerBase
+	def send_packet packet
+		send_raw packet.to_s
+	end
+
+	def on_parse
+		while packet = Packet.parse(@buffer_recv)
+			case packet.type
+			when Packet::TYPE_DEFAULT then on_packet  packet
+			when Packet::TYPE_CONTROL then on_control packet
+			end
+		end
+	end
+
+	def on_packet (packet); end
+	def on_control(packet); end
 end
